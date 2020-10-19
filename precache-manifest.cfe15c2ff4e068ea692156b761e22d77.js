90b8d0d9165b1b2978bd360a0d176e6c9d32dc5d
Yuriy Putsan <putsan57@gmail.com>
2020-10-19 16:02:21 +0300

    Updates

---
+++ precache-manifest.cfe15c2ff4e068ea692156b761e22d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca15668e9434ff0472d7ecb57b0fef9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bd908668a5c1127e54e",
+    "url": "./static/css/main.2157c98d.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd908668a5c1127e54e",
+    "url": "./static/js/main.9a1b7f95.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
